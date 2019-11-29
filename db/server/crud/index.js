27b2d6934c413c55f8c573b27d40d5a195ed530f
Tin Hoang <minhkies@gmail.com>
2019-11-29 00:04:00 -0800

    API help needed

--- db/server/crud/index.js
+++ db/server/crud/index.js
@@ -2,10 +2,12 @@ const createFunc = require("./create");
 const readFunc = require("./read");
 const updateFunc = require("./update");
 const deleteFunc = require("./delete");
+const customFunc = require("./custom");
 
 module.exports = {
   create:createFunc,
   read:readFunc,
   update:updateFunc,
-  delete:deleteFunc
+  delete:deleteFunc,
+  custom:customFunc
 }
