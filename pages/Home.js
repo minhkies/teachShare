db01e15d0929e526154a6760940ec5e4c3ad5692
Tin Hoang <minhkies@gmail.com>
2019-12-03 11:08:51 -0800

    Changes

--- pages/Home.js
+++ pages/Home.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {ScrollView} from 'react-native';
+import {ScrollView, Flatlist} from 'react-native';
 import PageTitle from '../comps/PageTitle';
 import HomeStyles from '../styles/HomeStyles';
 import SearchBar from '../comps/SearchBar';
@@ -11,14 +11,16 @@ import axios from 'axios';
 export default function Home() {
     let userProfile, teachingSubjects;
     let tempLessonPlans = [];
+    let temp = [];
     let [currentUser, setCurrentUser] = useState('');
     let [lessonPlans, setLessonPlans] = useState([]);
     let [subjects, setSubjects] = useState([]);
     let [selectedSubjects, setSelectedSubjects] = useState([]);
     let [stat, setStat] = useState();
+    let [c, setC] = useState(0);
 
     // const host = 'https://htin.postgres.database.azure.com:3001/post';
-    // const host = 'http://142.232.162.210:3001/post';
+    // const host = 'http://192.168.1.84:3001/post';
     // const host = 'http://192.168.1.90:3001/post';
     const host = 'https://teachsharek12ss.herokuapp.com/post';
 
@@ -39,10 +41,14 @@ export default function Home() {
                 userProfile = JSON.parse(userProfile);
                 teachingSubjects = JSON.parse(teachingSubjects);
                 setSubjects(teachingSubjects);
-                teachingSubjects.map((o, i) => {
+                teachingSubjects.map(() => {
                     tempSub.push(true);
                 });
-                await readLessonPlans(teachingSubjects, tempSub);
+                readLessonPlans(teachingSubjects, tempSub).then(async(r)=>{
+                    const ls = await readDependencies(r);
+                    setLessonPlans(ls);
+                    }
+                );
                 setSelectedSubjects(tempSub);
                 setCurrentUser(capitalize(userProfile.fname));
             }
@@ -51,48 +57,92 @@ export default function Home() {
         }
     };
 
-    let readLessonPlans = async (ts, sel) => {
-        tempLessonPlans = [];
-        lessonPlans = [];
-        let c = 0;
-        await ts.map(async (o, i) => {
-            if (sel[i] === true) {
-                c++;
-                let readLessonPlan = async () => {
-                    let data = await axios.post(host, {
-                        key: 'lesson_plans_read',
-                        data: {subject: o.subject, grade: o.grade},
-                    })
-                        .catch(function (error) {
-                        });
-                    let arr = JSON.parse(data.data.body).data;
-                    for (let z = 0; z < arr.length; z++) {
-                        let obj = arr[z];
-                        let x = await readCompetencies(obj.id.toString());
-                        obj['competencies'] = x;
-                        tempLessonPlans.push(obj);
-                    }
-                    console.log('templp', tempLessonPlans);
-                    return tempLessonPlans;
-                };
-
-                let tp = await readLessonPlan();
-                setLessonPlans(tp.map((o) => {
-                    return o;
-                }));
-            }
-        });
+    let compare = (a, b) => {
+        const idA = a.id;
+        const idB = b.id;
+        let comparison = 0;
+        if (idA > idB) {
+            comparison = 1;
+        } else {
+            comparison = -1;
+        }
+        return comparison;
     };
 
+    let readLessonPlans = async (t, s) =>{
+            return await Promise.all(t.map(async (o, i) => {
+                if (s[i] === true) {
+                    let d = await axios.post(host, {
+                        key: "lesson_plans_read_w_stats",
+                        data: {subject: o.subject.trim(), grade: o.grade, count: c}
+                    });
+                    return JSON.parse(d.data.body).data;
+                }
+            }));
+        };
 
-    let readCompetencies = async (id) => {
+    let readDependencies = async (r) =>{
+        let ls=r.flat().filter(Boolean).sort(compare).reverse();
+        for (let x=0; x<ls.length;x++){
+            ls[x]["competencies"] = await readCompetencies(ls[x]);
+        }
+        return ls;
+    };
 
+    let readCompetencies = async (o) => {
         let data = await axios.post(host, {
             key: 'competencies_read',
-            data: {pid: id},
+            data: {pid: o.id}
         }).catch(e => console.log(e.message));
         return JSON.parse(data.data.body).data;
     };
+
+    // let readLinks = async (o) => {
+    //     let data = await axios.post(host, {
+    //         key: 'links_read',
+    //         data: {pid: o.id}
+    //     }).catch(e => console.log(e.message));
+    //     return JSON.parse(data.data.body).data;
+    // };
+    // let readFiles = async (o) => {
+    //     let data = await axios.post(host, {
+    //         key: 'files_read',
+    //         data: {pid: o.id}
+    //     }).catch(e => console.log(e.message));
+    //     return JSON.parse(data.data.body).data;
+    // };
+    //
+    // let readAppreciations = async (o) => {
+    //     let data = await axios.post(host, {
+    //         key: 'appreciates_read',
+    //         data: {pid: o.id}
+    //     }).catch(e => console.log(e.message));
+    //     return JSON.parse(data.data.body).data;
+    // };
+    // let readViews = async (o) => {
+    //     let data = await axios.post(host, {
+    //         key: 'views_read',
+    //         data: {pid: o.id}
+    //     }).catch(e => console.log(e.message));
+    //     return JSON.parse(data.data.body).data;
+    // };
+    // let readDownloads = async (o) => {
+    //     let data = await axios.post(host, {
+    //         key: 'downloads_read',
+    //         data: {pid: o.id}
+    //     }).catch(e => console.log(e.message));
+    //     return JSON.parse(data.data.body).data;
+    // };
+    // let readCmts = async (o) => {
+    //     let data = await axios.post(host, {
+    //         key: 'cmts_read',
+    //         data: {pid: o.id}
+    //     }).catch(e => console.log(e.message));
+    //     return JSON.parse(data.data.body).data;
+    // };
+
+
+
     // let obj = {
     //     key: 'lesson_plans_read',
     //     data: {},
@@ -107,15 +157,35 @@ export default function Home() {
         getData();
     }, []);
 
+    console.log("hhhhhhhh", c);
+
     useEffect(() => {
-        readLessonPlans(teachingSubjects, selectedSubjects);
+        readLessonPlans(subjects, selectedSubjects).then(async(r)=>{
+                const ls = await readDependencies(r);
+                setLessonPlans(ls);
+                console.log(ls);
+            }
+        );
     }, [selectedSubjects]);
+
+    const detectBot = ({layoutMeasurement, contentOffset, contentSize}) => {
+        const paddingToBottom = 20;
+        return layoutMeasurement.height + contentOffset.y >=
+            contentSize.height - paddingToBottom;
+    };
 
     return (
         <ScrollView
             style={HomeStyles.wrapper}
             stickyHeaderIndices={[1]}
-            showsVerticalScrollIndicator={false}>
+            showsVerticalScrollIndicator={false}
+            onScroll={({nativeEvent}) => {
+                if (detectBot(nativeEvent)) {
+                    setC(c+=1);
+                }
+            }}
+            scrollEventThrottle={400}
+        >
             <PageTitle
                 title={'Welcome ' + currentUser}
                 msg={'This homepage is tailored for you!'}
@@ -142,6 +212,11 @@ export default function Home() {
                                 remarks={o.remarks}
                                 created_time={o.created_time.toLocaleString('en-US', {timeZone: 'Canada/Pacific'})}
                                 objs={o.learning_objs}
+                                coms={o.competencies}
+                                apps={o.c_apps}
+                                downs={o.c_downs}
+                                views={o.c_views}
+                                cmts={o.c_cmts}
                             />
                         );
                     }
