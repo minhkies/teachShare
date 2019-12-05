192b4b406f86b324fd0145c120b21532cdeda6cb
Tin Hoang <minhkies@gmail.com>
2019-12-05 14:07:44 -0800

    Revert "Navbar"
    This reverts commit 364249ebacba769f18a1cb821a7d70bc9524736f.
    
    # Conflicts:
    #	pages/MainRoute.js

--- pages/MainRoute.js
+++ pages/MainRoute.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {View, Text, BackHandler} from 'react-native';
 import {Router, Stack, Scene} from 'react-native-router-flux';
 
 import Home from './Home';
@@ -10,17 +11,44 @@ import Profile from './Profile';
 import Post from './Post';
 
 export default function Route(){
+
+    let [rN, setRN] = useState({
+        c: "home",
+        s: []
+    });
+    useEffect(()=>{
+        let backHandler = BackHandler.addEventListener('hardwareBackPress', (e)=>{
+
+        });
+    },[]);
     return(
-        <Router>
-            <Stack key="root" hideNavBar={true}>
-                <Scene key="home" component={Home} initial={true}/>
-                <Scene key="discover" component={Discover}/>
-                <Scene key="createPost" component={CreatePost}/>
-                <Scene key="notifications" component={Notifications}/>
-                <Scene key="more" component={More}/>
-                <Scene key="profile" component={Profile}/>
-                <Scene key="post" component={Post}/>
-            </Stack>
-        </Router>
+        <View style={{flex: 1}}>
+            <Router
+                backAndroidHandler={(e)=>{
+                    console.log("kkkkkkkk", e);
+                    if (e.index > 0){
+                        setRN(e.routes[e.index].routeName);
+                    }
+                }}
+                onStateChange={(e)=>{
+                console.log(e.routes[e.index].routeName);
+                setRN(e.routes[e.index].routeName);
+            }}>
+                <Stack key="root" hideNavBar={true}>
+                    <Scene key="home" component={Home} initial={rN==='home'}/>
+                    <Scene key="discover" component={Discover} initial={rN==='discover'}/>
+                    <Scene key="createPost" component={CreatePost} initial={rN==='createPost'}/>
+                    <Scene key="notifications" component={Notifications} initial={rN==='notifications'}/>
+                    <Scene key="more" component={More} initial={rN==='more'}/>
+                    <Scene key="profile" component={Profile}/>
+                    <Scene key="post" component={Post}/>
+                </Stack>
+            </Router>
+            <Navbar
+                OS={Platform.OS}
+                route={rN}
+            />
+        </View>
+
     );
 }
